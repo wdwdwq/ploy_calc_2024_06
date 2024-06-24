d7f654647a2c254c0f7c8832ca1e0ce84e584f5a
wdwdwq <qkenr7278@gmail.com>
2024-06-24 15:11:45 +0900

    3강 빼기

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,12 +2,43 @@ package org.example;
 
 public class Calc {
     public static int run(String exp) {
-        String[] bits = exp.split(" \\+ ");
+        boolean needToPlus = exp.contains("+");
+        boolean needToMinus = exp.contains("-");
+
+        String[] bits = null;
+
+        if (needToPlus) {
+            bits = exp.split(" \\+ ");
+        } else if (needToMinus) {
+            bits = exp.split(" \\- ");
+        }
 
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
 
-        return a + b;
+        if (needToPlus) {
+            return a + b;
+        } else if (needToMinus) {
+            return a - b;
+        }
+
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
 
     }
 }
+
+//String[] bits;
+//int result = 0;
+//
+//        if (exp.contains(" + ")) {
+//bits = exp.split(" \\+ ");
+//result = Integer.parseInt(bits[0].trim()) + Integer.parseInt(bits[1].trim());
+//        } else if (exp.contains(" - ")) {
+//bits = exp.split(" - ");
+//result = Integer.parseInt(bits[0].trim()) - Integer.parseInt(bits[1].trim());
+//        } else {
+//        throw new RuntimeException("지원하지 않는 연산입니다.");
+//        }
+//
+//                return result;
+
--- src/test/java/org/example/CalcTests.java
+++ src/test/java/org/example/CalcTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTests {
-
     @Test
     @DisplayName("1 + 1 == 2")
     void t1() {
@@ -24,11 +23,31 @@ public class CalcTests {
     void t3() {
         assertThat(Calc.run("2 + 2")).isEqualTo(4);
     }
+
     @Test
     @DisplayName("1000 + 200 == 1200")
     void t4() {
         assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
     }
+
+    @Test
+    @DisplayName("2 - 1 == 1")
+    void t5() {
+        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("3 - 1 == 2")
+    void t6() {
+        assertThat(Calc.run("3 - 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("100 - 20 == 80")
+    void t7() {
+        assertThat(Calc.run("100 - 20")).isEqualTo(80);
+    }
+    
 }
 
 //연산자 중간에는 띄어쓰기를 하는게 좋음 붙어있으면 가독성이 좋지않음
